5998562c3052c8623afc24ca1b656250860fb3e3
Davi Alves <davi_ti@outlook.com>
2025-12-06 16:46:35 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,29 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
+
+  const allowedOrigins = [
+    'http://localhost:3005',
+    'https://distribuidores-erp-back.vercel.app',
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3005',
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Origin not allowed by CORS'), false);
+    },
     credentials: true,
   });
+
+  // app.enableCors({
+  //   origin: 'http://localhost:3005',
+  //   credentials: true,
+  // });
   await app.listen(5555);
 }
 bootstrap();
