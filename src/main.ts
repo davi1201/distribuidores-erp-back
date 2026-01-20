31f9e5261ebbcc756d8b63eedca5b047dba28813
Davi Alves <davi_ti@outlook.com>
2026-01-20 16:36:33 -0300

    serveless

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser'; // Importação mais segura para TS
-import { RedisIoAdapter } from './adpters/redis.io.adapter';
+import cookieParser from 'cookie-parser';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+const server = express();
+
+export async function createApp() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
     rawBody: true,
   });
+
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
 
   app.enableCors({
     origin: (origin, callback) => {
-      // Permite requisições sem origem (ex: Postman, Mobile Apps ou Server-to-Server)
-      if (!origin) {
-        return callback(null, true);
-      }
+      if (!origin) return callback(null, true);
 
       const allowedOrigins = [
         'http://localhost:3000',
         'http://localhost:3005',
         'https://distribuidores-erp-front.vercel.app',
       ];
+
       const isAllowed =
         allowedOrigins.includes(origin) || origin.endsWith('.vercel.app');
 
-      if (isAllowed) {
-        callback(null, true);
-      } else {
-        console.warn(
-          `🚫 Bloqueio CORS: A origem ${origin} tentou acessar a API.`,
-        );
-        callback(new Error('Not allowed by CORS'));
-      }
+      if (isAllowed) callback(null, true);
+      else callback(new Error('Not allowed by CORS'));
     },
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With', // Adicionei headers comuns
   });
 
-  // CONFIGURAÇÃO DO REDIS ADAPTER
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis(); // Conecta no Redis antes de subir o app
-  app.useWebSocketAdapter(redisIoAdapter); // Aplica o adaptador
-
-  const port = process.env.PORT || 5555;
-  await app.listen(port);
+  await app.init();
+  return server;
 }
-bootstrap();
+
+export default createApp();
